58c93a9e06064e428f1d35fd66031842dfbed4d3
Rochelle Schofield <CodeGirl@macusers-MacBook-Pro.local>
2018-08-30 18:25:39 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+Friend Finder - Node & Express Servers
+GT Coding Bootcamp
+Overview
+In this activity, build a compatibility-based "FriendFinder" application -- sort of like a "dating app".
+
+This app has a Drag Queen theme. It will let the user submit a form of 10 questions and let them know which drag queen persona they best match up to. The app will display the image of the person with the best overall match.
+
+Technologies Used:
+HTML
+CSS
+Javascript
+Express
+Node
+JSON
+Skeleton CSS
+Body Parser
+
+Express was used to handle routing. Check out the app running on heroku here:
---
+++ app/data/friends.js
@@ -0,0 +1,46 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the possible friends.
+// ===============================================================================
+
+var friends = [
+  {
+    name: "Violet Chachki",
+    photo:
+      "https://akns-images.eonline.com/eol_images/Entire_Site/201817/rs_634x1024-180207100952-634-Violet-Chachki.jpg?fit=inside|900:650&output-quality=90",
+    scores: ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"]
+  },
+  {
+    name: "Bianca del Rio",
+    photo:
+      "https://akns-images.eonline.com/eol_images/Entire_Site/201817/rs_634x1024-180207100438-634-Bianca-Del-Rio--Season-6.jpg?fit=inside|900:650&output-quality=90",
+    scores: ["4", "2", "5", "1", "3", "2", "2", "1", "3", "2"]
+  },
+  {
+    name: "Alaska",
+    photo:
+      "https://akns-images.eonline.com/eol_images/Entire_Site/201817/rs_634x1024-180207102106-634-Alaska-rupaul.jpg?fit=inside|900:650&output-quality=90",
+    scores: ["5", "2", "2", "2", "4", "1", "3", "2", "5", "5"]
+  },
+  {
+    name: "Alyssa Edwards",
+    photo:
+      "https://akns-images.eonline.com/eol_images/Entire_Site/201817/rs_634x1024-180207111855-634-Alyssa-Edwards.jpg?fit=inside|900:650&output-quality=90",
+    scores: ["3", "3", "4", "2", "2", "1", "3", "2", "2", "3"]
+  },
+  {
+    name: "Sasha Velour",
+    photo:
+      "https://akns-images.eonline.com/eol_images/Entire_Site/201817/rs_634x1024-180207101223-634-Sasha-Velour.jpg?fit=inside|900:650&output-quality=90",
+    scores: ["4", "3", "4", "1", "5", "2", "5", "3", "1", "4"]
+  },
+  {
+    name: "Bob the Drag Queen",
+    photo:
+      "https://akns-images.eonline.com/eol_images/Entire_Site/201817/rs_634x1024-180207101130-634-Bob-the-Drag-Queen.jpg?fit=inside|900:650&output-quality=90",
+    scores: ["4", "4", "2", "3", "2", "2", "3", "2", "4", "5"]
+  }
+];
+
+// Here we export the array. This makes it accessible to other files using require.
+module.exports = friends;
